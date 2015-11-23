83e6039632774ee5cf2530ea52b3f6b127731322
Irina <issavchenko.2011@gmail.com>
2015-11-23 14:07:40 +0200

    Added information about_me

---
+++ js/script.js
@@ -0,0 +1,148 @@
+/**
+ * Created by Irina on 13-Nov-15.
+ */
+var dict = {
+    "edu_exp": {
+        ru: "Образование опыт",
+        en: "Education&experience",
+        ua: "Освіта та досвід"
+    },
+    "ab_me": {
+        ru: "Обо мне",
+        en: "About me",
+        ua: "Про мене"
+    },
+    "port": {
+        ru: "Портфолио",
+        en: "Portfolio",
+        ua: "Портфоліо"
+    },
+    "cont": {
+        en: "Contacts",
+        ru: "Контакты",
+        ua: "Контакти"
+    },
+    "resume_name": {
+        en: "Resume Savchenko Irina",
+        ru: "Резюме Савченко Ирины",
+        ua: "Резюме Савченко Ірини"
+    },
+    "main": {
+        en: "Main",
+        ru: "Главная",
+        ua: "Головна"
+
+    },
+    "name": {
+        en: "Savchenko Irina",
+        ru: "Савченко Ирина",
+        ua: "Савченко Ірина"
+    },
+    "date_of_birth": {
+        en: "<b>Date of birth:</b> 15-th of August 2015 (30 years)",
+        ru: "<b>Дата рождения:</b> 15 августа 2015 (30 лет)",
+        ua: "<b>Дата народження:</b> 15 серпня 2015 (30 років)"
+    },
+    "town": {
+        en: "<b>Town:</b> Kharkiv",
+        ru: "<b>Город</b> Харьков",
+        ua: "<b>Місто:</b> Харків"
+    },
+    "obj": {
+        en: "<b>Objective:</b> Getting a position of Trainee/Junior Front-End Developer",
+        ru: "<b>Цель:</b> Получение должности Trainee/Junior Front-End Developer",
+        ua: "<b>Ціль:</b> Отримання посади Trainee/Junior Front-End Developer"
+    },
+    "smr": {
+        en: "<b>Summary:</b> Theoretical knowledge and some practical experience in front-end solutions development. Good skills of HTML5 and CSS3. Well-informed about the different types of layout (сross-browser and adaptive),features of cascading in CSS. Some experience in using Git, SASS, JQuery and JQuery UI technologies. " +
+            "Familiar with responsive layout on Twitter Bootstrap. Ability to work with Adobe Photoshop.Try to write valid and semantic code and use newest technologies.",
+        ru: "<b>Резюме:</b> Теоретические знания и некоторый практический опыт в front-end разработке приложений. Хорошие навыки владения HTML5 и CSS3. Ознакомлена с различными видами верстки (кросс-браузерная и адаптивная верстка), особенностями каскадирования в CSS. Имею некоторый опыт в использовании Git, SASS, JQuery and JQuery UI технологий." +
+            " Знакома с responsive версткой на Twitter Bootstrap. Умею работать с Adobe Photoshop. Стараюсь писать валидный и семантический код и использовать новейшие технологии.",
+        ua: "<b>Резюме:</b> Теоретичні знання та деякий практичний досвід у front-end розробці. Гарні навички володіння HTML5 и CSS3. Ознайомлена з різними видами верстки (кросс-браузерня та адаптивная верстка), особливостями каскадування в CSS. Маю деякий досвід у використанні Git, SASS, JQuery and JQuery UI технологій." +
+            " Знайома з responsive версткой на Twitter Bootstrap. Вмію працювати з Adobe Photoshop. Намагаюся писати валідний та семантичний код та використовувати новітні технології."
+    },
+    "add_know": {
+        en: "<b>Additional knowledge:</b>",
+        ru: "<b>Дополнительные знания:</b>",
+        ua: "<b>Додаткові знання:</b>"
+    },
+    "op_syst": {
+        en: "<span>operating systems:</span>  MS Windows, Linux (Ubuntu)",
+        ru: "<span>операционные системы:</span>   MS Windows, Linux (Ubuntu)",
+        ua: "<span>операційні системи:</span>   MS Windows, Linux (Ubuntu)"
+    },
+    "bag_track": {
+        en: "<span>bug trackers:</span> Jira, Mantis, Bugzilla, Redmine",
+        ru: "<span>баг-трекеры:</span>  Jira, Mantis, Bugzilla, Redmine",
+        ua: "<span>баг-трекери:</span> Jira, Mantis, Bugzilla, Redmine"
+    },
+    "database": {
+        en: "<span>basic knowledge of databases and SQL queries:</span> MySql, MySql Workbench",
+        ru: "<span>основы баз данных и SQL запросов:</span> MySql, MySql Workbench",
+        ua: "<span>основи баз даних та SQL запитів:</span> MySql, MySql Workbench"
+    },
+    "mod_meth": {
+        en: "<span>models and methodologies of software development:</span> Scrum /XP / Kanban / Agile",
+        ru: "<span>модели и методологии разработки ПО:</span> Scrum /XP / Kanban / Agile",
+        ua: "<span>моделі та методологіі розробки ПО:</span> Scrum /XP / Kanban / Agile"
+    },
+    "req": {
+        en: "<span>different types of software requirements</span>",
+        ru: "<span>различные типы требований к ПО</span>",
+        ua: "<span>різноманітні типи вимог до ПО</span>"
+    },
+    "lang": {
+        en: "<b>Languages:</b>",
+        ru: "<b>Языки:</b>",
+        ua: "<b>Язики:</b>"
+    },
+    "lang_en": {
+        en: "<span>English:</span> (pre-intermediate) continue studying and improving knowledge",
+        ru: "<span>Английский:</span> (pre-intermediate) продолжаю изучать и углублять знания",
+        ua: "<span>Англійська:</span> (pre-intermediate) продовжую вивчати і поглиблювати знання"
+    },
+    "lang_ru": {
+        en: "<span>Russian:</span> native",
+        ru: "<span>Русский:</span> родной",
+        ua: "<span>Російська:</span> рідна"
+    },
+    "lang_ua": {
+        en: "<span>Ukrainian:</span> native",
+        ru: "<span>Украинский:</span> родной",
+        ua: "<span>Українська:</span> рідна"
+    },
+    "about_me_edu": {
+        en: "<b>Education:</b>",
+        ru: "<b>Образование:</b>",
+        ua: "<b>Освіта:</b>"
+    },
+    "qa": {
+        en: "<span>2014 - </span>course QA (Source IT company)",
+        ru: "<span>2014 - </span>курсы QA (компания Source IT )",
+        ua: "<span>2014 - </span>курси QA (компанія Source IT)"
+    },
+    "en-class": {
+        en: "<span>2013 - present time</span> English courses (language school «Enjoy»)",
+        ru: "<span>2013 - настоящее время</span> курсы английского языка (школа «Enjoy»)",
+        ua: "<span>2013 - теперішній час</span> курси англійської мови(школа «Enjoy»)"
+    },
+    "univer": {
+        en: "<span>2002-2007</span> V.N. Karazin Kharkiv National University, Mechanics and Mathematics department, specialization: Mathematics. Master`s degree in Mathematics and Computer Science",
+        ru: "<span>2002-2007</span> ХНУ им.В.Н.Каразина, механико-математический факультет, специализация: математика. Магистр математики и информатики.",
+        ua: "<span>2002-2007</span> ХНУ ім.В.Н.Каразіна, механіко-математичний факультет, спеціалізація: математика. Магістр математики та інформатики."
+    }
+};
+
+$(document).ready(function() {
+    $('body').translate({lang: "en", t: dict}); // initialize the plugin and translate the entire page body
+    $("b").css("color","#2D1F6F");
+    $("li span").css("color","#2D1F6F");
+    $("p").css("text-align","justify");
+    $('#language').change(function () { //choose language
+        $('body').translate({lang: $(this).val(), t: dict});
+        $("b").css("color","#2D1F6F");
+        $("li span").css("color","#2D1F6F");
+        $("p").css("text-align","justify");
+    });
+
+});
